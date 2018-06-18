3c4a7a61f475f25cf6c062c761f1e55194bebd33
Apple Princess <yharuhi396@gmail.com>
2018-06-19 00:01:40 +0100

    Basic Login/Logout UI created.

--- background.js
+++ background.js
@@ -10,60 +10,86 @@ let receivedAny = false;
 const HomepageURL = 'tk2-217-18218.vs.sakura.ne.jp';
 const ChatPort = 8891;
 let WS = null;
+let timerId = undefined;
 const queue = [];
 function notify(options, callback) {
   const defaultNotificationOption = {title:'', message:'', type:'basic', 'iconUrl':'./icon.png'};
-  browserRoot.notifications.create('', {...defaultNotificationOption, ...options},
-                                   (notificationId) => callback(notificationId));
+  browserRoot.notifications.create(
+    '',
+    {...defaultNotificationOption, ...options},
+    (notificationId) => callback(notificationId));
 }
 
 browserRoot.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if(request.close !== undefined){
     WS.close();
-    sendResponse({status:2, message:'Websocket is closed.'});
+    // we want to use sendResponse here.
+    // but for firefox compatibility issue, we cannot. at least AFAIK
+    clearInterval(timerId);
+    timerId = undefined;
+    WS = null;
+    browserRoot.runtime.sendMessage({status:2, message:'Websocket is closed.'});
+    receivedAny = false;
+    queue.length = 0;
+    //sendResponse({status:2, message:'Websocket is closed.'});
+    return;
+  }
+  if(request.checkingStatus !== undefined){
+    if(WS == null){
+      browserRoot.runtime.sendMessage({status: 3, message: ''});
+    } else {
+      browserRoot.runtime.sendMessage({status: 4, message: ''});
+    }
     return;
   }
   WS = new WebSocket(`ws://${HomepageURL}:${ChatPort}`);
-  WS.addEventListener('open', (event) => {
+  WS.addEventListener('open', () => {
     WS.send(`\t${request.uid}\t${request.passwd}`);
   });
   WS.addEventListener('message', (event) => {
     if(event.data === 'ID又はPASSが違います')
     {
       WS.close();
-      sendResponse({status:-1, message: 'ID or Password is incorrect.'});
+      browserRoot.runtime.sendMessage({status:-1, message: 'ID or Password is incorrect.'});
       return;
     }
     if(!receivedAny) {
-      sendResponse({status:0, message:'Websocket is OK.'});
+      browserRoot.runtime.sendMessage({status:0, message:'Websocket is OK.'});
+      //sendResponse({status:0, message:'Websocket is OK.'});
       receivedAny = true;
     }
-    queue.push(new Chat(event.data).toNotifiable(), (e) => {});
+    queue.push(new Chat(event.data).toNotifiable(), () => {});
   });
-  WS.addEventListener('close', (event) => {
-    console.log(event);
+  WS.addEventListener('close', () => {
+    // console.log(event);
     WS = null;
-  })
+    clearInterval(timerId);
+    timerId = undefined;
+    receivedAny = false;
+    queue.length = 0;
+  });
 
   // ボタン以外のエリアが押された時
-  browserRoot.notifications.onClicked.addListener((nid) => {
+  browserRoot.notifications.onClicked.addListener(() => {
     browserRoot.tabs.create({url:'http://' + HomepageURL}, () => {});
-  })
+  });
 
   //ボタンが押された時 ( 通知バーに出てくるあいつはボタンという扱いらしい。)
-  browserRoot.notifications.onButtonClicked.addListener((nid) => {
+  browserRoot.notifications.onButtonClicked.addListener(() => {
     browserRoot.tabs.create({url:'http://' + HomepageURL}, () => {});
-  })
+  });
   // it can be as sensitive as one notification per one messsage, but, for here, once per 30s
-  setInterval(() => {
-    if(queue.length !== 0)
-    {
-      notify({message:'You got new message.'}, id => {
-        console.log(id);
-      });
-      queue.length = 0; // queue.clear;
-    }
-  }, 30000);
+  if (timerId === undefined) {
+    timerId = setInterval(() => {
+      if(queue.length !== 0)
+      {
+        notify({message:'You got new message.'}, () => {
+        // console.log(id)
+        });
+        queue.length = 0; // queue.clear;
+      }
+    }, 30000);
+  }
 });
 
 class Chat
--- popup.js
+++ popup.js
@@ -1,5 +1,4 @@
 // this script is relying on the fact that it is loaded after DOM is fully loaded.
-const messageBox = document.querySelector('#message');
 
 let browserRoot;
 if(chrome !== undefined) {
@@ -7,13 +6,12 @@ if(chrome !== undefined) {
 } else if (browser !== undefined) {
   browserRoot = browser;
 }
+
 document.querySelector('#submit').addEventListener('click', () => {
   const uid = document.querySelector('#uid').value;
   const passwd = document.querySelector('#passwd').value;
   if(uid === '' || passwd === '') {
-    const messageBox = document.querySelector('#message');
-    messageBox.innerHTML = 'ID and Password cannot be empty.';
-    messageBox.style.visibility = 'visible';
+    toggleMessageBox('ID and Password cannot be empty.');
     return;
   }
   browserRoot.runtime.sendMessage({uid, passwd});
@@ -21,24 +19,56 @@ document.querySelector('#submit').addEventListener('click', () => {
 
 browserRoot.runtime.onMessage.addListener((request) => {
   if(request.status === -1) {
-    messageBox.innerHTML = request.message;
-    messageBox.style.visibility = 'visible';
+    toggleMessageBox(request.message);
   }
-  if(request.status === 0) {
-    messageBox.innerHTML = '';
-    messageBox.style.visibility = 'hidden';
-    document.querySelector('#submitO').style.visibility = 'visible';
-    document.querySelector('#submit').style.visibility = 'hidden';
+
+  if (request.status === 0) {
+    toggleMessageBox();
+    toggleLoginButton();
     document.querySelector('#submitO').addEventListener('click', () => {
       browserRoot.runtime.sendMessage({close:1});
     });
   }
-  if(request.status === 1) {
+
+  if (request.status === 1) {
     window.open(request.destination, request.target);
   }
-  if(request.status === 2) {
-    document.querySelector('#submit').style.visibility = 'visible';
-    document.querySelector('#submitO').style.visibility = 'hidden';
 
+  if (request.status === 2) {
+    toggleLoginButton();
   }
+  if (request.status == 3) {
+    if (document.querySelector('#submit').style.visibility === 'hidden') {
+      toggleLoginButton();
+    }
+  }
+  if (request.status == 4) {
+    if (document.querySelector('#submitO').style.visibility === 'hidden') {
+      document.querySelector('#submitO').addEventListener('click', () => {
+        browserRoot.runtime.sendMessage({close:1});
+      });
+      toggleLoginButton();
+    }
+  }
+
 });
+
+browserRoot.runtime.sendMessage({checkingStatus: true});
+
+function toggleLoginButton() {
+  const loginBtn = document.querySelector('#submit');
+  const logoutBtn = document.querySelector('#submitO');
+  loginBtn.style.visibility = (loginBtn.style.visibility === 'visible' ? 'hidden' : 'visible');
+  logoutBtn.style.visibility = (logoutBtn.style.visibility === 'visible' ? 'hidden' : 'visible');
+}
+
+function toggleMessageBox(msg) {
+  const messageBox = document.querySelector('#message');
+  if(msg === undefined) {
+    messageBox.innerHTML = '';
+    messageBox.style.visibility = 'hidden';
+  } else {
+    messageBox.innerHTMl = msg;
+    messageBox.style.visibility = 'visible';
+  }
+}
